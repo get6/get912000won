7c838fb4b1eebc976ca5e69ebfb2e41e2572a378
slate <hhwang@student.42seoul.kr>
2024-01-27 19:05:47 +0900

    Change ThemeToggle.tsx code simply

--- app/ui/ThemeToggle.tsx
+++ app/ui/ThemeToggle.tsx
@@ -6,19 +6,14 @@ import { useTheme } from "next-themes"
 function ToggleIcon() {
   const { theme, setTheme } = useTheme()
 
-  if (theme === "dark") {
-    return (
-      <MoonIcon
-        className="h-6 w-6 hover:cursor-pointer dark:text-gray-400"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      />
-    )
-  }
   return (
-    <SunIcon
-      className="h-6 w-6 hover:cursor-pointer dark:text-gray-400"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    />
+    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      {theme === "dark" ? (
+        <MoonIcon className="h-6 w-6 hover:cursor-pointer dark:text-gray-400" />
+      ) : (
+        <SunIcon className="h-6 w-6 hover:cursor-pointer dark:text-gray-400" />
+      )}
+    </button>
   )
 }
 
