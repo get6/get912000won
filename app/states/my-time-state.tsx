345ebda18ca46e7046caacf564e8f5965de09caa
get6 <ittae.com@gmail.com>
2024-01-28 01:21:50 +0900

    Add default state

--- app/states/my-time-state.tsx
+++ app/states/my-time-state.tsx
@@ -1,10 +1,19 @@
 import { atom, selector } from "recoil"
 
+const FINISH_TIME = 160
+export const MAX_TARGET_HOURS = 12
+
+export const finishTimeState = atom<number>({
+  key: "finishTimeState",
+  default: FINISH_TIME,
+})
+
 export const remainingHoursState = selector<number>({
   key: "remainingHoursState",
   get: ({ get }) => {
+    const finishTime = get(finishTimeState)
     const cumulativeTime = get(cumulativeHoursState)
-    return 160 - cumulativeTime
+    return finishTime - cumulativeTime
   },
 })
 
@@ -15,5 +24,5 @@ export const cumulativeHoursState = atom<number>({
 
 export const targetHoursState = atom<number>({
   key: "targetHoursState",
-  default: 12,
+  default: MAX_TARGET_HOURS,
 })
