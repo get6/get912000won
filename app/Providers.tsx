ae23fcc38cd6373a9a39707c5f099bf57822089b
slate <hhwang@student.42seoul.kr>
2024-01-31 17:26:36 +0900

    Fix: theme toggle icon rendering issue #7

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,9 +1,18 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
+import { useEffect, useState } from "react"
 import { RecoilRoot } from "recoil"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [isMount, setMount] = useState(false)
+
+  useEffect(() => {
+    setMount(true)
+  }, [])
+
+  if (!isMount) return null
+
   return (
     <ThemeProvider attribute="class">
       <RecoilRoot>{children}</RecoilRoot>
--- app/ui/ThemeToggle.tsx
+++ app/ui/ThemeToggle.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { FaceSmileIcon } from "@heroicons/react/16/solid"
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline"
 import { useTheme } from "next-themes"
 
 function ToggleIcon() {
-  const { theme, setTheme } = useTheme()
+  const { systemTheme, theme, setTheme } = useTheme()
+
+  const currentTheme = theme === "system" ? systemTheme : theme
 
   return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      {theme === "dark" ? (
+    <button
+      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
+    >
+      {currentTheme === "dark" ? (
         <MoonIcon className="h-6 w-6 hover:cursor-pointer dark:text-gray-400" />
       ) : (
         <SunIcon className="h-6 w-6 hover:cursor-pointer dark:text-gray-400" />
