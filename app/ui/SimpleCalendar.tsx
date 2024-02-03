17b912f8c636a3314a1aafbf4106f45ae64a1ce4
slate <hhwang@student.42seoul.kr>
2024-02-03 18:03:10 +0900

    Feat: Make button to select/unselect all dates

--- app/ui/SimpleCalendar.tsx
+++ app/ui/SimpleCalendar.tsx
@@ -65,50 +65,65 @@ export default function SimpleCalendar() {
   }
 
   return (
-    <div className="for-mobile max-w-sm rounded-lg bg-gray-50 p-3 py-8 shadow-lg dark:bg-gray-700">
-      <h2 className="flex-auto text-center text-gray-900 dark:text-white">
-        🗓️ {format(lastDayOfMonth, "M")}월 남은 날 {availableDates.length}일
-      </h2>
-      <div className="mt-8 grid grid-cols-7 text-center">
-        {weekDays.map((day, index) => (
-          <div
-            key={index}
-            className="text-sm font-semibold leading-6 text-amber-300 dark:text-blue-400"
-          >
-            {day}
-          </div>
-        ))}
+    <div>
+      <div className="flex justify-center pb-5 text-xs font-semibold">
+        <button
+          onClick={() => setSelectedDates(availableDates)}
+          className="selected-button mx-2 flex h-9 w-28 items-center justify-center rounded-lg"
+        >
+          모든 날 선택
+        </button>
+        <button
+          onClick={() => setSelectedDates([])}
+          className="unselected-button mx-2 flex h-9 w-28 items-center justify-center rounded-lg bg-gray-200 dark:bg-gray-700"
+        >
+          모든 날 선택 해제
+        </button>
       </div>
-      <div className="mt-2 grid grid-cols-7 text-sm">
-        {days.map((day, dayIdx) => (
-          <div
-            key={day.toString()}
-            className={classNames(
-              dayIdx === 0 && colStartClasses[getDay(day)],
-              "item-center px-0.5 py-0.5"
-            )}
-          >
-            <button
-              disabled={!isAvailableDay(day)}
-              onTouchEnd={() => datePickMobile(day)}
-              onClick={() => datePick(day)}
+      <div className="for-mobile max-w-sm rounded-lg bg-gray-50 p-3 py-8 shadow-lg dark:bg-gray-700">
+        <h2 className="flex-auto text-center text-gray-900 dark:text-white">
+          🗓️ {format(lastDayOfMonth, "M")}월 남은 날 {availableDates.length}일
+        </h2>
+        <div className="mt-8 grid grid-cols-7 text-center">
+          {weekDays.map((day, index) => (
+            <div
+              key={index}
+              className="text-sm font-semibold leading-6 text-amber-300 dark:text-blue-400"
+            >
+              {day}
+            </div>
+          ))}
+        </div>
+        <div className="mt-2 grid grid-cols-7 text-sm">
+          {days.map((day, dayIdx) => (
+            <div
+              key={day.toString()}
               className={classNames(
-                !isAvailableDay(day) && "disabled: cursor-default opacity-30",
-                isAvailableDay(day) &&
-                  !selectedDates.includes(day) &&
-                  "text-gray-900 hover:bg-gray-300 dark:text-gray-300 hover:dark:bg-gray-500",
-                isWeekend(day) && "text-red-500 dark:text-red-500",
-                selectedDates.includes(day) &&
-                  "bg-amber-300 text-gray-100 hover:bg-amber-200 dark:bg-blue-600 hover:dark:bg-blue-500",
-                "flex h-9 w-9 items-center justify-center rounded-lg font-semibold"
+                dayIdx === 0 && colStartClasses[getDay(day)],
+                "item-center px-0.5 py-0.5"
               )}
             >
-              <time dateTime={format(day, "yyyy-MM-dd")}>
-                {format(day, "d")}
-              </time>
-            </button>
-          </div>
-        ))}
+              <button
+                disabled={!isAvailableDay(day)}
+                onTouchEnd={() => datePickMobile(day)}
+                onClick={() => datePick(day)}
+                className={classNames(
+                  !isAvailableDay(day) && "disabled: cursor-default opacity-30",
+                  isAvailableDay(day) &&
+                    !selectedDates.includes(day) &&
+                    "unselected-button",
+                  isWeekend(day) && "text-red-500 dark:text-red-500",
+                  selectedDates.includes(day) && "selected-button",
+                  "flex h-9 w-9 items-center justify-center rounded-lg font-semibold"
+                )}
+              >
+                <time dateTime={format(day, "yyyy-MM-dd")}>
+                  {format(day, "d")}
+                </time>
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
